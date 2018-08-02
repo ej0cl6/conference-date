433eca6b68363ce85c12e81e1e8449d146a92ad4
ej0cl6 <ej0cl6@gmail.com>
2018-08-02 15:14:41 +0800

    update info

--- data.js
+++ data.js
@@ -2,13 +2,22 @@ var label = ["conference", "abstract", "paper", "notification", "date", "locatio
 var check = ["abstract", "paper", "notification"];
 var data = [
 	{
-		"conference": "AAAI 2019",
-		"abstract": "2018/09/01",
-		"paper": "2018/09/05",
-		"notification": "2018/11/01",
-		"date": "2019/01/27 - 2019/02/01",
-		"location": "Honolulu, Hawaii, USA",
-		"link": "https://aaai.org/Conferences/AAAI-19/"
+		"conference": "ICML 2019",
+		"abstract": "",
+		"paper": "",
+		"notification": "",
+		"date": "2019/06/10 - 2019/06/15",
+		"location": "Long Beach, California, USA",
+		"link": "https://icml.cc/Conferences/2019"
+	},
+	{
+		"conference": "NIPS 2018",
+		"abstract": "",
+		"paper": "2018/05/18",
+		"notification": "2018/09/01",
+		"date": "2018/12/03 - 2018/12/08",
+		"location": "Montreal, Canada",
+		"link": "https://nips.cc/Conferences/2018"
 	},
 	{
 		"conference": "ICLR 2018",
@@ -20,33 +29,6 @@ var data = [
 		"link": "https://iclr.cc/Conferences/2018"
 	},
 	{
-		"conference": "SDM 2019",
-		"abstract": "",
-		"paper": "",
-		"notification": "",
-		"date": "2019/05/02 - 2019/05/04",
-		"location": "Calgary, Alberta, Canada",
-		"link": "http://www.siam.org/meetings/sdm19/"
-	},
-	{
-		"conference": "NAACL 2018",
-		"abstract": "",
-		"paper": "2017/12/15",
-		"notification": "2018/02/14",
-		"date": "2018/06/01 - 2018/06/06",
-		"location": "New Orleans, Louisiana, USA",
-		"link": "http://naacl2018.org/"
-	},
-	{
-		"conference": "PAKDD 2019",
-		"abstract": "",
-		"paper": "2018/10/10",
-		"notification": "2018/12/15",
-		"date": "2019/04/15 - 2019/04/18",
-		"location": "Macau, China",
-		"link": "http://pakdd2019.medmeeting.org/Content/91973"
-	},
-	{
 		"conference": "CVPR 2019",
 		"abstract": "",
 		"paper": "2018/11/16",
@@ -54,24 +36,6 @@ var data = [
 		"date": "2019/06/15 - 2019/06/21",
 		"location": "Long Beach, California, USA",
 		"link": "http://cvpr2019.thecvf.com/"
-	},
-	{
-		"conference": "ICML 2019",
-		"abstract": "",
-		"paper": "",
-		"notification": "",
-		"date": "2019/06/10 - 2019/06/15",
-		"location": "Long Beach, California, USA",
-		"link": "https://icml.cc/Conferences/2019"
-	},
-	{
-		"conference": "IJCAI 2018",
-		"abstract": "2018/01/25",
-		"paper": "2018/01/31",
-		"notification": "2018/04/16",
-		"date": "2018/07/13 - 2018/07/19",
-		"location": "Stockholm, Sweden",
-		"link": "http://www.ijcai-18.org/"
 	},
 	{
 		"conference": "ACL 2019",
@@ -83,33 +47,6 @@ var data = [
 		"link": "http://acl2019.org/"
 	},
 	{
-		"conference": "KDD 2018",
-		"abstract": "",
-		"paper": "2018/02/11",
-		"notification": "2018/05/06",
-		"date": "2018/08/19 - 2018/08/23",
-		"location": "London, United Kingdom",
-		"link": "http://www.kdd.org/kdd2018/"
-	},
-	{
-		"conference": "ECCV 2018",
-		"abstract": "",
-		"paper": "2018/03/14",
-		"notification": "2018/07/01",
-		"date": "2018/09/08 - 2018/09/14",
-		"location": "Munich, Germany",
-		"link": "https://eccv2018.org/"
-	},
-	{
-		"conference": "ECML 2018",
-		"abstract": "2018/04/05",
-		"paper": "2018/04/12",
-		"notification": "2018/06/14",
-		"date": "2018/09/10 - 2018/09/14",
-		"location": "Dublin, Ireland",
-		"link": "http://www.ecmlpkdd2018.org/"
-	},
-	{
 		"conference": "EMNLP 2018",
 		"abstract": "",
 		"paper": "2018/05/22",
@@ -119,13 +56,31 @@ var data = [
 		"link": "http://emnlp2018.org/"
 	},
 	{
-		"conference": "ACML 2018",
+		"conference": "AAAI 2019",
+		"abstract": "2018/09/01",
+		"paper": "2018/09/05",
+		"notification": "2018/11/01",
+		"date": "2019/01/27 - 2019/02/01",
+		"location": "Honolulu, Hawaii, USA",
+		"link": "https://aaai.org/Conferences/AAAI-19/"
+	},
+	{
+		"conference": "IJCAI 2018",
+		"abstract": "2018/01/25",
+		"paper": "2018/01/31",
+		"notification": "2018/04/16",
+		"date": "2018/07/13 - 2018/07/19",
+		"location": "Stockholm, Sweden",
+		"link": "http://www.ijcai-18.org/"
+	},
+	{
+		"conference": "KDD 2018",
 		"abstract": "",
-		"paper": "2018/08/01",
-		"notification": "2018/09/20",
-		"date": "2018/11/14 - 2018/11/16",
-		"location": "Beijing, China",
-		"link": "http://acml-conf.org/2018/"
+		"paper": "2018/02/11",
+		"notification": "2018/05/06",
+		"date": "2018/08/19 - 2018/08/23",
+		"location": "London, United Kingdom",
+		"link": "http://www.kdd.org/kdd2018/"
 	},
 	{
 		"conference": "ICDM 2018",
@@ -137,13 +92,67 @@ var data = [
 		"link": "http://icdm2018.org/"
 	},
 	{
-		"conference": "NIPS 2018",
+		"conference": "ICCV 2019",
 		"abstract": "",
-		"paper": "2018/05/18",
-		"notification": "2018/09/01",
-		"date": "2018/12/03 - 2018/12/08",
-		"location": "Montreal, Canada",
-		"link": "https://nips.cc/Conferences/2018"
+		"paper": "",
+		"notification": "",
+		"date": "2019/10/27 - 2019/11/03",
+		"location": "Seoul, Korea",
+		"link": "http://iccv2019.thecvf.com/"
+	},
+	{
+		"conference": "ECCV 2018",
+		"abstract": "",
+		"paper": "2018/03/14",
+		"notification": "2018/07/01",
+		"date": "2018/09/08 - 2018/09/14",
+		"location": "Munich, Germany",
+		"link": "https://eccv2018.org/"
+	},
+	{
+		"conference": "NAACL 2019",
+		"abstract": "",
+		"paper": "",
+		"notification": "",
+		"date": "2019/06/02 - 2019/06/07",
+		"location": "Minneapolis, Minnesota, USA",
+		"link": "http://naacl2019.org/"
+	},
+	{
+		"conference": "ECML 2018",
+		"abstract": "2018/04/05",
+		"paper": "2018/04/12",
+		"notification": "2018/06/14",
+		"date": "2018/09/10 - 2018/09/14",
+		"location": "Dublin, Ireland",
+		"link": "http://www.ecmlpkdd2018.org/"
+	},
+	{
+		"conference": "SDM 2019",
+		"abstract": "2018/10/05",
+		"paper": "2018/10/12",
+		"notification": "",
+		"date": "2019/05/02 - 2019/05/04",
+		"location": "Calgary, Alberta, Canada",
+		"link": "http://www.siam.org/meetings/sdm19/"
+	},
+	{
+		"conference": "PAKDD 2019",
+		"abstract": "",
+		"paper": "2018/10/10",
+		"notification": "2018/12/15",
+		"date": "2019/04/15 - 2019/04/18",
+		"location": "Macau, China",
+		"link": "http://pakdd2019.medmeeting.org/Content/91973"
+	},
+	{
+		"conference": "ACML 2018",
+		"abstract": "",
+		"paper": "2018/08/01",
+		"notification": "2018/09/20",
+		"date": "2018/11/14 - 2018/11/16",
+		"location": "Beijing, China",
+		"link": "http://acml-conf.org/2018/"
 	},
 ];
 
